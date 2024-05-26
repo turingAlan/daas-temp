36351053f4c6893180b724143c4b8c257c3e4a7e
turingAlan <sarthak2107597@st.jmi.ac.in>
2024-05-27 03:44:05 +0530

    fix

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,14 +1,14 @@
-import { BoxesLoader } from "react-awesome-loaders";
 import { colors } from "../data/colors";
+import { InfinitySpin } from "react-loader-spinner";
 
 const Loader = () => {
   return (
     <div className="absolute w-screen h-screen flex items-center justify-center bg-black bg-opacity-20 z-50 backdrop-blur-sm">
-      <BoxesLoader
-        boxColor={colors.chartPurple}
-        style={{ marginBottom: "20px" }}
-        desktopSize={"128px"}
-        mobileSize={"80px"}
+      <InfinitySpin
+        visible={true}
+        width="200"
+        color={colors.chartPurple}
+        ariaLabel="infinity-spin-loading"
       />
     </div>
   );
